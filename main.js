6547ee29a8b894e0098c82eee0ec7b242ed451a9
coderrahil <86824681+coderrahil@users.noreply.github.com>
2023-04-19 19:46:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function changecolor(){
+    document.getElementById("r").style.color="blue";
+}
+function changeimage(){
+    document.getElementById("s").src="https://www.racefans.net/wp-content/uploads/2022/02/racefansdotnet-2022-02-17_135752.jpg";
+}
